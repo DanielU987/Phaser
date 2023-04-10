596e8dba338e400439500296cf58aa79a4a7a5fd
Daniel Ussov <danikusov@yandex.ru>
2023-04-10 11:46:25 +0300

    moving chest

--- src/Player.js
+++ src/Player.js
@@ -1,19 +1,35 @@
 import Phaser from "phaser";
-export class Player extends Phaser.GameObjects.Sprite{
-    constructor(scene,x,y) {
-        super(scene,x,y,'player');
+export class Player extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, 'player');
         this.setTexture('atlas', 'chest_empty_open_anim');
         this.setScale(4);
-        ley key[
-        let keyA= this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCodes.A)
-        let keyD= this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCodes.D)
-        let keyW= this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCodes.W)
-        let keyS= this.input.keyboard.addkey(Phaser.Input.Keyboard.KeyCodes.S)
-        ]
+        this.maxSpeed = 300;
+        this.keys = {
+            a: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+            d: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+            w: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+            s: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+        }
     }
+    move(key,delta){
+        let oppositeCheck = key.keyCode == 65 || key.keyCode == 68 ? (this.keys.s.isDown || this.keys.w.isDown) : (this.keys.a.isDown || this.keys.d.isDown)
+        let axis = key.keyCode == 65 || key.keyCode == 68 ? 'x' : 'y';
+        let direction = key.keyCode == 83 || key.keyCode == 68 ? 1: -1;
 
-
-    update(){
-        
+        if(key.isDown){
+            if(oppositeCheck){
+                this[axis] += direction * Math.sqrt((this.maxSpeed * this.maxSpeed) / 2) / 1000 * delta;
+            }else{
+                this[axis] += direction * this.maxSpeed / 1000 * delta;
+            }
+        }
+    }
+    preUpdate(time, delta) {
+    super.preUpdate(time,delta)
+    this.move(this.keys.a,delta)
+    this.move(this.keys.d,delta)
+    this.move(this.keys.w,delta)
+    this.move(this.keys.s,delta)
     }
 }
